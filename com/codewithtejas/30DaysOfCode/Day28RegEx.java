a12a7cae796f7837c498d97d2fbf6174e8637cd7
Tejas Subramani <44034397+subramani-tejas@users.noreply.github.com>
2021-09-26 13:30:01 -0400

    Create Day28RegEx.java

---
+++ com/codewithtejas/30DaysOfCode/Day28RegEx.java
@@ -0,0 +1,39 @@
+/*
+  https://www.hackerrank.com/challenges/30-regex-patterns/problem
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+
+        String emailRegEx = ".+@gmail\\.com$";
+        Pattern pattern = Pattern.compile(emailRegEx);
+
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            String name = in.next();
+            String email = in.next();
+            Matcher matcher = pattern.matcher(email);
+
+            if (matcher.find()) list.add(name);
+        }
+        Collections.sort(list);
+        list.forEach(System.out::println);
+    }
+}
