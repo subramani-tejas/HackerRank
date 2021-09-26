ede12171e7e77c4b5160445dfa54f825a39972da
Tejas Subramani <44034397+subramani-tejas@users.noreply.github.com>
2021-09-26 13:15:39 -0400

    Create CamelCase.java

---
+++ com/codewithtejas/problem_solving/strings/CamelCase.java
@@ -0,0 +1,55 @@
+/*
+  https://www.hackerrank.com/challenges/camelcase/problem
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'camelcase' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int camelcase(String str) {
+        // Write your code here
+        int count = 0;
+
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) >= 65 && str.charAt(i) <= 90)
+                count++;
+        }
+        return count + 1;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        int result = Result.camelcase(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
