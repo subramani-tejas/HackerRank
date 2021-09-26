b3a29a4a700ea85a901c01a73422e6f0695541e2
Tejas Subramani <44034397+subramani-tejas@users.noreply.github.com>
2021-09-26 13:35:07 -0400

    Create Day26NestedLogic.java

---
+++ com/codewithtejas/30DaysOfCode/Day26NestedLogic.java
@@ -0,0 +1,39 @@
+/*
+  https://www.hackerrank.com/challenges/30-nested-logic/problem
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner scanner = new Scanner(System.in);
+
+        // date returned
+        int dayReturned = scanner.nextInt();
+        int monthReturned = scanner.nextInt();
+        int yearReturned = scanner.nextInt();
+
+        // date due
+        int dayDue = scanner.nextInt();
+        int monthDue = scanner.nextInt();
+        int yearDue = scanner.nextInt();
+
+        scanner.close();
+        int fine = 0;
+        
+        if (yearReturned < yearDue) { }
+        else{
+            if (yearReturned > yearDue) fine = 10000;
+            else if (monthReturned > monthDue)
+                fine = 500 * (monthReturned - monthDue);
+            else if (dayReturned > dayDue)
+                fine = 15 * (dayReturned - dayDue);
+        }
+
+        System.out.println(fine);
+    }
+}
