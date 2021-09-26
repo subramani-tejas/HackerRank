6948d24d8c10553e2bbbcf3bfbdf142b845b3ee0
Tejas Subramani <44034397+subramani-tejas@users.noreply.github.com>
2021-09-25 21:10:10 -0500

    Create CompareTheTriplets.java

---
+++ com/codewithtejas/warmup/CompareTheTriplets.java
@@ -0,0 +1,36 @@
+/*
+https://www.hackerrank.com/challenges/compare-the-triplets/problem
+*/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static void solve(int a0, int a1, int a2, int b0, int b1, int b2) {
+        // Complete this function
+        int pointsAlice = ((a0 > b0) ? 1 : 0) + ((a1 > b1) ? 1 : 0) + ((a2 > b2) ? 1 : 0);
+        int pointsBob = ((a0 < b0) ? 1 : 0) + ((a1 < b1) ? 1 : 0) + ((a2 < b2) ? 1 : 0);
+        System.out.println(pointsAlice + " " + pointsBob);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a0 = in.nextInt();
+        int a1 = in.nextInt();
+        int a2 = in.nextInt();
+        int b0 = in.nextInt();
+        int b1 = in.nextInt();
+        int b2 = in.nextInt();
+        solve(a0, a1, a2, b0, b1, b2);
+      /*  for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + (i != result.length - 1 ? " " : ""));
+        }
+        System.out.println("");
+        */
+
+    }
+}
